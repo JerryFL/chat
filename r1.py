c7df098dc936e34d42bc0e96e215363da5287e08
JerryFL <3573764836@qq.com>
2020-01-21 15:28:40 +0800

    revision 1

--- r1.py
+++ r1.py
@@ -1,25 +1,32 @@
 
 def read_file(filename):
-	lines = []
-	with open(filename, 'r', encoding = 'utf-8-sig') as f:
-		for line in f:
-			lines.append(line.strip())
-	return lines
+    lines = []
+    with open(filename, 'r', encoding = 'utf-8-sig') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
 
 def convert(lines):
-	new = []
-	for line in lines:
+    new = []
+    person = None
+    for line in lines:
         if line == 'Jerry':
-    		person = 'Jerry'
-    		continue
-    	elif line == 'Laurance':
-    		person = 'Laurance'
-    		continue
-    	new.append(person + ':' + line)
-        print(new)
+            person = 'Jerry'
+            continue
+        elif line == 'Laurance':
+            person = 'Laurance'
+            continue
+        if person:
+            new.append(person + ':' + line)
+    return new
 
+def write_file(filename, lines):
+    with open(filename, 'w')as f:
+        for line in lines:
+            f.write(line + '\n')
+            write_file(output.txt)
 
 def main():
     lines = read_file('chat.txt')
-    convert(lines)
+    lines = convert(lines)
 main()
